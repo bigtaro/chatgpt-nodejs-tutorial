9a3958e1d50c72fb5597c6b86418c3dc4959a0e1
andrew xu <abc@abc.com>
2023-02-13 09:14:04 +0800

    update text

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -10,7 +10,7 @@ export default async function (req, res) {
     model: "text-davinci-003",
     prompt: generatePrompt(req.body.animal),
     temperature: 0.6,
-	max_tokens: 1000
+	  max_tokens: 1000
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
--- pages/index.js
+++ pages/index.js
@@ -29,16 +29,16 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>ChatGPT Q&A</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Enter your queston"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Submit question" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
